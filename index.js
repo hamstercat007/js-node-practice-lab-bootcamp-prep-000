bc4ce3a8cdd5ed215ead89e911a9fe1c61f636c3
Lisa Mac <lisamac421@gmail.com>
2018-05-03 10:56:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 // write your code below!
 
 var name = "Miaowsky"
-var height = 158
+var height = "158"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height+1)
+console.log(parseInt(height)+1)
